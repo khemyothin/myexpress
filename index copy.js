4526ecd5dc733f24ef93e34d1265954bd2942990
khemyothin <teww2024@gmail.com>
2026-05-15 22:06:17 +0700

    Create index copy.js

---
+++ index copy.js
@@ -0,0 +1,54 @@
+import * as line from '@line/bot-sdk'
+import express from 'express'
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+// create LINE SDK config from env variables
+const config = {
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = line.LineBotClient.fromChannelAccessToken({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
+});
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create an echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [echo],
+  });
+}
+
+// listen on port
+const port = process.env.PORT || 3020;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
